7f63178f7d898b0951cdf84a01c0c8430a51eac4
Bhavesh <bhaveshdaswani93@gmail.com>
2025-01-24 13:38:57 +0530

    initial commmit

---
+++ challenges/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+  'january': 'Jan Challenge',
+  'feburary': 'Feb Challenge',
+  'march': 'March Challenge',
+  'april': 'April Challenge',
+  'may': 'May Challenge',
+  'june': 'June Challenge',
+  'july': 'July Challenge',
+  'august': 'August Challenge',
+  'september': 'September Challenge',
+  'october': 'October Challenge',
+  'november': 'November Challenge',
+  'december': 'December Challenge'
+}
+
+# Create your views here.
+def index(request):
+  return HttpResponse('<p>Hello world</p>')
+
+def feb(request):
+  return HttpResponse('<p>Feb Month </p>')
+  
+def monthly_challenge_by_number(request, month):
+  month_name_list = list(monthly_challenge.keys())
+  
+  if month > len(month_name_list):
+    return HttpResponseNotFound('Month not found')
+  
+  month_name = month_name_list[month - 1]
+  return redirect('/challenges/'+month_name)
+  #return HttpResponse(f'month number {month}')
+  
+def monthly_challenge(request, month):
+   try:
+    month_res =  monthly_challenge[month]
+      return HttpResponse(res)
+    except:
+       return HttpResponseNotFound('Monthly Challenge not fount')
+  
+  
